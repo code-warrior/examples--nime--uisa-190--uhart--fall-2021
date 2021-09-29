17c19d7379e6e259d1d3f4ed9906d359d387cb5a
Roy A Vanegas <roy@thecodeeducators.com>
2021-09-29 16:01:20 -0400

    Add Arduino sketch that reads a potentiometer

---
+++ untitled folder/read-a-potentiometer/read-a-potentiometer.ino
@@ -0,0 +1,23 @@
+/**
+ * Read Analog Values From a Potentiometer
+ * ----------------------------------------------------------------------------------
+ *
+ * Read the values from a potentiometer connected to analog pin 2. Make sure to open
+ * Arduino’s Serial Monitor: Tools | Serial Monitor
+ */
+
+#define POTENTIOMETER 2      // Connect the potentiometer — or pot — to analog PIN 2.
+#define BAUD_RATE     9600   // Set baud rate used by the serial connection to 9600.
+
+int potentiometer_value = 0; // Hold the value read from the pot.
+
+void setup() {
+  Serial.begin(BAUD_RATE);   // Establish the baud rate for the serial connection.
+}
+
+void loop() {
+  potentiometer_value = analogRead(POTENTIOMETER); // Read the pot’s value, then...
+  Serial.println(map(analogRead(A2), 0, 674, 60, 90));             // print to the serial monitor.
+  
+  delay(100);
+}
